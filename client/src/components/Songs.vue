e7f8fb92231e0e0570f9999c6b7b5dabb4874355
julien charton <jul.charton@gmail.com>
2019-02-20 18:55:25 +1100

    Link from songs to song working

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -22,48 +22,58 @@
       </v-flex> -->
         <v-flex xs12 md5 offset-md3>
           <panel title="Songs">
-              <router-link :to="{name:  'create-song'}" slot="action">
-                <v-btn
-                class="red darken-1 mb-2"
-                fab dark absolute small right bottom
-                >
-                <v-icon>add</v-icon>
-                </v-btn>
-              </router-link>
+            <router-link :to="{name:  'create-song'}" slot="action">
+              <v-btn
+              class="red darken-1 mb-2"
+              fab dark absolute small right bottom
+              >
+              <v-icon>add</v-icon>
+              </v-btn>
+            </router-link>
             <div class="mt-3">
-              <v-card color="#B74F6F" class="white--text mb-2"
-              v-for="song in songs"
-              :key="song.id">
-                <v-layout row>
-                  <v-flex xs7>
-                    <v-card-title primary-title class="song-title">
-                      <div>
-                        <div class="headline">{{song.title}}</div>
-                        <div>{{song.artist}}</div>
-                        <div>{{song.album}}</div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-layout align-center justify-end pa-2>
-                      <v-img
-                        :src="song.albumImage" 
-                        max-height="125px"
-                        contain />
-                    </v-layout>
-                  </v-flex>
-                </v-layout>
-              <!-- <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Rate this album
-                <v-spacer></v-spacer>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-              </v-card-actions> -->
-              </v-card>
+              <v-hover v-for="song in songs"
+                  :key="song.id" id="pointer">
+                <v-card color="#B74F6F" class="white--text mb-2" 
+                  slot-scope="{ hover }" 
+                  :class="`elevation-${hover ? 12 : 2}`"
+                  @click="navigateTo({
+                  name: 'song', 
+                  params: {
+                    songId: song.id
+                    }
+                  })"
+                >
+                  <v-layout row align-center >
+                    <v-flex xs7>
+                      <v-card-title  class="song-title" >
+                        <div>
+                          <div class="headline">{{song.title}}</div>
+                          <div>{{song.artist}}</div>
+                          <div>{{song.album}}</div>
+                        </div>
+                      </v-card-title>
+                    </v-flex>
+                    <v-flex xs5>
+                      <v-layout align-center justify-end pa-2>
+                        <v-img
+                          :src="song.albumImage" 
+                          max-height="125px"
+                          contain />
+                      </v-layout>
+                    </v-flex>
+                  </v-layout>
+                <!-- <v-divider light></v-divider>
+                <v-card-actions class="pa-3">
+                  Rate this album
+                  <v-spacer></v-spacer>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                  <v-icon>star_border</v-icon>
+                </v-card-actions> -->
+                </v-card>
+              </v-hover>
             </div>
           </panel> 
         </v-flex>
@@ -84,11 +94,18 @@ export default {
   },
   async mounted () {
     this.songs = (await SongsService.index()).data
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
   }
 }
 </script>
 <style>
-  
+  /* #pointer {
+  cursor: pointer;
+} */
 </style>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
